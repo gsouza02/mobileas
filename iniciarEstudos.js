d80e20c31b424a4f64823b9e78b6c8a5b78b5dc3
Gabriel Souza <souza.gabriel1870@gmail.com>
2024-05-28 17:23:39 -0300

    mais um

---
+++ iniciarEstudos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function IniciarEstudos() {
+  return (
+        <View>
+            <Text>FAZER A TELA PF</Text>
+        </View>
+  );
+};
